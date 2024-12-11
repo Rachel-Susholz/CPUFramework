8415ca2149b38ef0335e91c06259ef5937130122
Rachel-Susholz <rochelsusholz@gmail.com>
2024-12-11 11:39:12 -0500

    added allownulls

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -20,7 +20,19 @@ namespace CPUFramework
             cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+            SetAllColumnsAllowNull(dt);
             return dt;
+        }
+        public static void ExecuteSQL(string sqlstatrment)
+        {
+            GetDataTable(sqlstatrment);
+        }
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach(DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            } 
         }
 
         /*public static void DebugPrintDataTable(DataTable dt)
